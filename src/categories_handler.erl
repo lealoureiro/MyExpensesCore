763300d8bb2e0272a6b8ce14d96b9852c1effdfd
lealoureiro@gmail.com <lealoureiro@gmail.com>
2016-06-26 14:36:15 +0200

    improving output structure

--- src/categories_handler.erl
+++ src/categories_handler.erl
@@ -64,8 +64,9 @@ get_json(Req, State) ->
           JSON = jiffy:encode({InitialData}),
           {JSON, Req, State};
         [_ | _] ->
-          Result = dict:to_list(fill_sub_categories(SubCategories, InitialData)),
-          JSON = jiffy:encode({Result}),
+          Result = fill_sub_categories(SubCategories, InitialData),
+          Result2 = final_output_mapper(dict:fetch_keys(Result), Result),
+          JSON = jiffy:encode(Result2),
           {JSON, Req, State};
         _ ->
           {false, Req, State}
@@ -88,6 +89,7 @@ fill_sub_categories([SubCategory | T], Dict) ->
 
   Category = proplists:get_value(category_name, SubCategory),
   SubCategoryName = proplists:get_value(name, SubCategory),
+
   try
     SubCategoryList = dict:fetch(Category, Dict),
     fill_sub_categories(T, dict:store(Category, [SubCategoryName] ++ SubCategoryList, Dict))
@@ -96,6 +98,13 @@ fill_sub_categories([SubCategory | T], Dict) ->
       lager:log(warning, self(), "Category ~s not found for sub category ~s!", [Category, SubCategoryName]),
       fill_sub_categories(T, Dict)
   end.
+
+final_output_mapper([], _) ->
+  [];
+
+final_output_mapper([H | T], Dict) ->
+  SubCategoriesList = dict:fetch(H, Dict),
+  [{[{name, H}, {subCategories, SubCategoriesList}]}] ++ final_output_mapper(T, Dict).
 
 process_post(Req, State) ->
   {ClientId, _} = cowboy_req:meta(<<"clientId">>, Req),
